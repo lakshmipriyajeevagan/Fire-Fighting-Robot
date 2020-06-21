7c6f536142424806542bf6f283d0b974e51a1a5f
Lakshmi Priya Jeevagan <42070220+lakshmipriyajeevagan@users.noreply.github.com>
2020-06-21 18:45:00 +0530

    Program for GSM

--- gsmffr.py
+++ gsmffr.py
@@ -2,9 +2,9 @@ import serial
 import time, sys
 import datetime
 P_BUTTON = 24
-SERIAL_PORT = "/dev/ttyS0"  #Raspberry Pi 3
+SERIAL_PORT = "/dev/ttyS0"  # Raspberry Pi 3
 ser = serial.Serial(SERIAL_PORT,baudrate = 9600,timeout = 5)
-ser.write("AT + CGMF = 1\r")  #set to text mode
+ser.write("AT + CGMF = 1\r")  # set to text mode
 time.sleep(3)
 a=0
 while True:
@@ -20,5 +20,5 @@ while True:
         msg = "FIRE DETECTED at " + t +":--" + state
         print ("Sending SMS with status info: " + msg)
         ser.write(msg + chr(26)) 
-        ser.write("\x1A")  #hex digits
+        ser.write("\x1A")  # hex digits
         time.sleep(3)
